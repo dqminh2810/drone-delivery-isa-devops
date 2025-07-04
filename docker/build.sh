b8add55ab62ad8b3bb0bfe0b58cc670a19d494fc
dqminh2810 <dqminh2810@gmail.com>
2025-07-04 11:23:18 +0700

    update docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Be sure that you do not have started mvn tomee:run or mono server.exe
 
-#cd clients
-#./build.sh
-#cd ..
+cd partners
+./build.sh
+cd ..
 
 cd dd
 ./build.sh
 cd ..
 
-cd partners
+cd clients
+./build.sh
+cd ..
+
+cd jenkins
 ./build.sh
 cd ..
